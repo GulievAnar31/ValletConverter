d0d434e9dc5fc5f598e2992e77891b673201e89e
guliev <guliev@internet.ru>
2022-09-14 17:19:18 +0300

    try req =(

--- src/components/CurrencyBlock/Currency.tsx
+++ src/components/CurrencyBlock/Currency.tsx
@@ -3,8 +3,46 @@ import { LogicComponent } from '../LogicComponent';
 import styles from './Currency.module.scss';
 import Usd from '../../images/usd.svg.png';
 import ChangeVallet from '../../images/bitcoin.svg';
+import axios from 'axios';
+
+const TARGET_ASSET_ID = "b2384bf2-b14d-4916-aa97-85633ef05742";
+
+export interface DataType {
+  id: string;
+  source_currency: string;
+  target_crypto_asset_id: string;
+  source_amount: string;
+  target_amount: string;
+  fiat_blockchain_fee: string;
+  absolute_iternal_fee: string;
+  iternal_fee_percent: string;
+  total_fee: string;
+  expires_at: Date;
+}
 
 export const CurrencyComponent: React.FC = () => {
+  const [pay, setPay] = React.useState(100);
+  const [recive, setRecive] = React.useState(0);
+
+  async function fetchCreateQuote(value: number, isSource: boolean) {
+    try {
+      const { data }: any = axios.post<DataType>('https://api-qjoa5a5qtq-uc.a.run.app/quotes', {
+        source_currency: "USD",
+        target_crypto_asset_id: TARGET_ASSET_ID,
+        [isSource ? 'source_amount' : 'target_amount']: '100.0',
+      });
+
+      if (isSource) setRecive(Number(data.target_amount))
+      else setPay(Number(data.source_amount));
+    } catch (err) {
+      console.warn(err);
+      alert('Error creating');
+    }
+  }
+
+  React.useEffect(() => {
+    fetchCreateQuote(pay, false);
+  }, []);
 
   return <>
     <div className={styles.root}>
@@ -13,13 +51,15 @@ export const CurrencyComponent: React.FC = () => {
         subtitle='You Pay'
         currencyName='Usd'
         currencyIcon={Usd}
-        value={100}
+        value={pay}
+        onChangeValue={setPay}
       />
       <LogicComponent
         subtitle='You Recive'
         currencyName='Usdc Evmos'
         currencyIcon={ChangeVallet}
         value={100}
+        onChangeValue={setRecive}
       />
       <button>Buy Now</button>
     </div>
--- src/components/LogicComponent/index.tsx
+++ src/components/LogicComponent/index.tsx
@@ -6,16 +6,16 @@ interface LogicBlockProps {
   currencyName: string;
   currencyIcon: string;
   value: number;
-  onChangeValue?: (val: number) => void;
+  onChangeValue: (val: number) => void;
 }
 
 export const LogicComponent: React.FC<LogicBlockProps> = (props) => {
-  const { subtitle, value, currencyName, currencyIcon } = props
+  const { subtitle, value, currencyName, currencyIcon, onChangeValue } = props
 
   return <div className={styles.currency}>
     <p>{subtitle}</p>
     <div className={styles.bottom}>
-      <input defaultValue={value} className={styles.input} />
+      <input type={'number'} onChange={(e) => onChangeValue(Number(e.target.value))} defaultValue={value} className={styles.input} />
       <div className={styles.currencyInfo}>
         <h5>{currencyName}</h5>
         <img src={currencyIcon} alt="ChangeVallet" width={50} height={50} />
